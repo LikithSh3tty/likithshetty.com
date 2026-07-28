8bbba13023e48112bdb0e48d667cf842630b1af6
lucifer360 <75750335+lucifer360@users.noreply.github.com>
2026-07-29 02:26:33 +0530

    add type-to-navigate aliases for the new destinations

--- src/modules/pagesIndex.js
+++ src/modules/pagesIndex.js
@@ -12,12 +12,36 @@ export const PAGES = [
     url: '/websites/',
   },
   {
+    aliases: ['resume', 'cv'],
+    url: '/resume',
+    external: true,
+  },
+  {
+    aliases: ['writing', 'blog', 'medium', 'posts', 'articles'],
+    url: 'https://medium.com/@shettylikith.rajesh',
+    external: true,
+  },
+  {
+    aliases: ['playlists', 'music', 'songs', 'listening'],
+    url: '/playlists/',
+  },
+  {
     aliases: ['home', 'index'],
     url: '/',
   },
   {
     aliases: ['github', 'repositories', 'code'],
     url: 'https://github.com/LikithSh3tty',
+    external: true,
+  },
+  {
+    aliases: ['linkedin', 'work', 'hire'],
+    url: 'https://linkedin.com/in/likith-shetty',
+    external: true,
+  },
+  {
+    aliases: ['email', 'mail', 'contact', 'hi'],
+    url: 'mailto:likithshetty188@gmail.com',
     external: true,
   },
 ]
